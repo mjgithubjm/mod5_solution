9f65984c11823c0d6ae828cea6bcb9a70dfbcf26
mjgithubjm <marija.jelenkovic@gmail.com>
2017-02-19 18:48:34 +0100

    Version 2

--- src/public/user/signup.js
+++ src/public/user/signup.js
@@ -9,13 +9,14 @@ RegistrationController.$inject = ['$http', 'ApiPath', 'MenuService'];
 function RegistrationController($http, ApiPath, MenuService) {
   var reg = this;
   reg.menuService = MenuService;
+  reg.noSuchItem = false;
 
-  reg.submit = function () {
-  	console.log('in submit');
+  reg.submit = function () {  	
   	reg.menuService.getMenuItem(reg.user.shortname).then(function (response) {
   		console.log(response);
   		if (response.short_name){
   			console.log('true');
+  			reg.noSuchItem = false;
   			reg.completed = true;
   			reg.menuService.firstname = reg.user.firstname;
   			reg.menuService.lastname = reg.user.lastname;
@@ -25,8 +26,9 @@ function RegistrationController($http, ApiPath, MenuService) {
   			reg.saved = true;
   		}  		     
     })
-    .catch(function (error) {
-    	console.log(error);
+    .catch(function (error) {    	
+    	reg.noSuchItem = true;
+    	console.log(reg.noSuchItem);
       	reg.completed = false;
     })    
   };
